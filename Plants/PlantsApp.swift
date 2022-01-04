d0c51f230b1afbb7fb3c24d05efebf9cb51f09cd
Victoria Bermudez <vmbermudez044@gmail.com>
2022-01-04 08:01:11 -0500

    Initial Commit

---
+++ Plants/PlantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantsApp.swift
+//  Plants
+//
+//  Created by Victoria Bermudez (student LM) on 1/4/22.
+//
+
+import SwiftUI
+
+@main
+struct PlantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
